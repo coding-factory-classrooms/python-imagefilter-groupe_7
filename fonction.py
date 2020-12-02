d1f80024911ddad36658a6dbaa24ef148125bba7
mattmax1 <matthieukoebsch@hotmail.com>
2020-12-02 16:56:18 +0100

    def et cli

---
+++ fonction.py
@@ -0,0 +1,55 @@
+import cv2
+import os
+import numpy as np
+
+#TODO rajouter un if pour filtrer les extensions fichiers
+
+def gray (dossierE , dossierS):
+    files = os.listdir(dossierE)
+
+    for f in files:
+        img = cv2.imread(f"{dossierE}/{f}")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(f"{dossierS}/{f}", gray)
+
+
+
+
+def flou(dossierE, dossierS):
+    files = os.listdir(dossierE)
+
+    for f in files:
+        img = cv2.imread(f"{dossierE}/{f}")
+
+        try:
+            flou = cv2.GaussianBlur(img, (5, 11), cv2.BORDER_DEFAULT)
+        except cv2.error as e:
+            print('taille de flou pair ou image pas en jpg/png')
+
+        try:
+            cv2.imwrite(f"{dossierS}/{f}", flou)
+        except NameError as e:
+            print('varible pas crée')
+        except cv2.error as e:
+            print('')
+
+
+
+def latter(dossierE, dossierS):
+    files = os.listdir(dossierE)
+    kernel = np.ones((10, 10), np.uint(8))
+
+    for f in files:
+        img = cv2.imread(f"{dossierE}/{f}")
+
+        try:
+            latter = cv2.dilate(img, kernel, 20)
+        except cv2.error as e:
+            print('image pas en jpg/png')
+
+        try:
+            cv2.imwrite(f"{dossierS}/{f}", latter)
+        except NameError as e:
+            print('varible pas crée')
+        except cv2.error as e:
+            print('')
--- main.py
+++ main.py
@@ -1,57 +1,26 @@
-import cv2
-import os
-import numpy as np
+from fonction import gray, flou, latter
+import sys
 
-
-#separe dans notre logique dossier entree/sortie et fichier
 try:
     dossier_entrance = "images"
     dossier_exit = "images2.0"
-    #1 ouvrir un dossier qui contient des images
 
-    files = os.listdir(dossier_entrance)
-
-    #2 pous chacune des images les ouvrir avec opencv, appliquer le filtre , enregistrer l'image dans le dossier de sortie
-
-    for f in files:
-        #TODO rajouter un if pour filtrer les extensions fichiers
-        img = cv2.imread(f"{dossier_entrance}/{f}")
-        kernel = np.ones((10, 10),np.uint(8))
+    args = sys.argv
 
 
-        try:
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        except cv2.error as e:
-            print('image pas en jpg/png')
 
-
-        try:
-            flou = cv2.GaussianBlur(img,(5,11),cv2.BORDER_DEFAULT)
-        except cv2.error as e:
-            print('taille de flou pair ou image pas en jpg/png')
-
-
-        try:
-            latter = cv2.dilate(img, kernel, 20)
-        except cv2.error as e:
-            print('')
-
-
-        try:
-           cv2.imwrite(f"{dossier_exit}/{f}", flou)
-        except NameError as e:
-            print('varible pas crée')
-        except cv2.error as e:
-            print('')
-
+    for i in range(0, len(args)):
+        arg = args[i]
+        print(arg)
+        if arg == 'gray':
+            gray(dossier_entrance,dossier_exit)
+        elif arg == 'flou':
+            flou(dossier_exit, dossier_exit)
+        elif arg == 'latter':
+            latter(dossier_entrance, dossier_exit)
 
 except FileNotFoundError as e:
     print('dossier inexistant')
-
-
-
-
-
 
 
 

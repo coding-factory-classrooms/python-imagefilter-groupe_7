bcbeb28de60b7c9d430a5b0c299a54618a04ccb8
TortueBleu <klarir.games@gmail.com>
2020-12-02 12:16:26 +0100

    tkt

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import cv2
+import os
+import numpy as np
 
-
-def gray(img):
+#separe dans notre logique dossier entree/sortie et fichier
+dossier_entrance = "images"
+dossier_exit = "images2.0"
+#1 ouvrir un dossier qui contient des images
+files = os.listdir(dossier_entrance)
+#2 pous chacune des images les ouvrir avec opencv, appliquer le filtre , enregistrer l'image dans le dossier de sortie
+for f in files:
+    print(f)
+    #TODO rajouter un if pour filtrer les extensions fichiers
+    img = cv2.imread(f"{dossier_entrance}/{f}")
+    kernel = np.ones((10, 10),np.uint(8))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Original images', image1)
-    cv2.imshow('Gray images', gray)
-    
-    return gray
-
-
-
-image1 = cv2.imread("/Users/admin/python-imagefilter-groupe_7/images/armin.png")
-image2 = cv2.imread("/Users/admin/python-imagefilter-groupe_7/images/thesaitama.png")
+    flou = cv2.medianBlur(img, 15)
+    latter = cv2.dilate(img, kernel, 20)
+    cv2.imwrite(f"{dossier_exit}/{f}", latter)
 
 
 
 
 
 
-cv2.imwrite("/Users/admin/python-imagefilter-groupe_7/images2.0/armin.png", gray)
 
 
 

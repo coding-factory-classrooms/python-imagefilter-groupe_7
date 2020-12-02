d182370914f7509282bca7ff17027ff7f18eed65
TortueBleu <klarir.games@gmail.com>
2020-12-02 11:12:47 +0100

    paumer

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import cv2
 
+
+def gray(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Original images', image1)
+    cv2.imshow('Gray images', gray)
+    
+    return gray
+
+
+
 image1 = cv2.imread("/Users/admin/python-imagefilter-groupe_7/images/armin.png")
-gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+image2 = cv2.imread("/Users/admin/python-imagefilter-groupe_7/images/thesaitama.png")
 
-cv2.imshow('Original images', image1)
-cv2.imshow('Gray images', gray)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+
+
+
+cv2.imwrite("/Users/admin/python-imagefilter-groupe_7/images2.0/armin.png", gray)
+
 
 

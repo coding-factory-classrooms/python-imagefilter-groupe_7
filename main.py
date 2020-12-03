a141c534d5a0e571eaea4fd594e791970551018e
mattmax1 <matthieukoebsch@hotmail.com>
2020-12-03 10:06:44 +0100

    probleme de arg[1]

--- main.py
+++ main.py
@@ -6,34 +6,35 @@ try:
     dossier_exit = "images2.0"
 
     args = sys.argv
-    print(args)
-    if args[1] == 'gray':
-        gray(dossier_entrance,dossier_exit)
-        for i in range(2, len(args)):
-            arg = args[i]
-            if arg == 'flou':
-                flou(dossier_exit, dossier_exit)
-            elif arg == 'latter':
-                latter(dossier_exit,dossier_exit)
+    try:
+        if args[1] == 'gray':
+            gray(dossier_entrance,dossier_exit)
+            for i in range(2, len(args)):
+                arg = args[i]
+                if arg == 'flou':
+                    flou(dossier_exit, dossier_exit)
+                elif arg == 'latter':
+                    latter(dossier_exit,dossier_exit)
 
-    elif args[1] == 'flou':
-        flou(dossier_entrance, dossier_exit)
-        for i in range(2, len(args)):
-            arg = args[i]
-            if arg == 'gray':
-                gray(dossier_exit, dossier_exit)
-            elif arg == 'latter':
-                latter(dossier_exit,dossier_exit)
+        elif args[1] == 'flou':
+            flou(dossier_entrance, dossier_exit)
+            for i in range(2, len(args)):
+                arg = args[i]
+                if arg == 'gray':
+                    gray(dossier_exit, dossier_exit)
+                elif arg == 'latter':
+                    latter(dossier_exit,dossier_exit)
 
-    elif args[1] == 'latter':
-        latter(dossier_entrance, dossier_exit)
-        for i in range(2, len(args)):
-            arg = args[i]
-            if arg == 'flou':
-                flou(dossier_exit, dossier_exit)
-            elif arg == 'gray':
-                gray(dossier_exit,dossier_exit)
-
+        elif args[1] == 'latter':
+            latter(dossier_entrance, dossier_exit)
+            for i in range(2, len(args)):
+                arg = args[i]
+                if arg == 'flou':
+                    flou(dossier_exit, dossier_exit)
+                elif arg == 'gray':
+                    gray(dossier_exit,dossier_exit)
+    except IndexError as e:
+        print('mettre une option de filtre')
 except FileNotFoundError as e:
     print('dossier inexistant')
 
